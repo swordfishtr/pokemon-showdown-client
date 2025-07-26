e7b1ce9e93201c3a0029fc827b95570b61755fd6
demir <demir.ab@protonmail.com>
2025-07-26 12:07:34 +0300

    Fix in-battle avatar scaling

--- play.pokemonshowdown.com/src/battle-animations.ts
+++ play.pokemonshowdown.com/src/battle-animations.ts
@@ -723,7 +723,7 @@ export class BattleScene implements BattleSceneStub {
 		}
 		return (
 			`<div class="trainer trainer-${posStr}"${faded}><strong>${BattleLog.escapeHTML(side.name)}</strong>` +
-			`<div class="trainersprite"${ratinghtml} style="background-image:url(${Dex.resolveAvatar(side.avatar)})">` +
+			`<div class="trainersprite"${ratinghtml} style="background-image:url(${Dex.resolveAvatar(side.avatar)});background-repeat:no-repeat;background-size:80px;">` +
 			`</div>${badgehtml}${pokemonhtml}</div>`
 		);
 	}
