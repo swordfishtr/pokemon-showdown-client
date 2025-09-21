e2f292f1bc41de89eda57c9d2f39da8c45837813
demir <demir.ab@protonmail.com>
2025-09-21 12:15:50 +0300

    Preact: Debug

--- play.pokemonshowdown.com/src-gens/battle-dex-search.ts
+++ play.pokemonshowdown.com/src-gens/battle-dex-search.ts
@@ -106,11 +106,13 @@ export class GTTIndex {
 
 		const overrides = this.format.overrideSpeciesData;
 		if(overrides && (speciesid in overrides)) {
+			console.log('override');
 			moddedSpecies ??= structuredClone(species);
 			Object.assign(moddedSpecies, overrides[speciesid]);
 		}
 
 		if(this.format.flipped) {
+			console.log('flipped');
 			moddedSpecies ??= structuredClone(species);
 			const reversedNums = Object.values(moddedSpecies.baseStats).reverse();
 			for (const [i, statName] of Object.keys(moddedSpecies.baseStats).entries()) {
@@ -120,6 +122,7 @@ export class GTTIndex {
 
 		// TODO: check mod priority
 		if(this.format.scalemons) {
+			console.log('scalemons');
 			moddedSpecies ??= structuredClone(species);
 			const bstWithoutHp: number = moddedSpecies.bst - moddedSpecies.baseStats['hp'];
 			const scale = 600 - moddedSpecies.baseStats['hp'];
