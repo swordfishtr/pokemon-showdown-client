b6ba5464439b5c8172243223a02f330b58bc5aa8
demir <demir.ab@protonmail.com>
2025-10-24 02:26:19 +0300

    fix?

--- play.pokemonshowdown.com/src-gens/client-endload.ts
+++ play.pokemonshowdown.com/src-gens/client-endload.ts
@@ -1,12 +1,17 @@
 import { PS } from './client-main';
-import { enableDragDropTouch } from '../js-gens/lib/drag-drop-touch.esm.js';
 
-enableDragDropTouch(undefined, undefined, {
-	isPressHoldMode: true,
-});
+setTimeout(() => {
+	if(window.DragDropTouch) {
+		console.log('Polyfill DragDropTouch');
+		window.DragDropTouch.enable(undefined, undefined, {
+			isPressHoldMode: true,
+		});
+	}
 
-PS.libsLoaded.loaded();
+	PS.libsLoaded.loaded();
+}, 1);
 
+// Showdex loads very slowly, so we don't count it towards `PS.libsLoaded`
 if(PS.prefs.showdex) {
 	const script = document.createElement('script');
 	script.src = 'showdex-gens/main.js';
