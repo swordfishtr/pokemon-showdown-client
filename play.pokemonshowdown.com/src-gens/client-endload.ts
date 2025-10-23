9a9bdeca967373c709ee219f50b7133f07480019
demir <demir.ab@protonmail.com>
2025-10-23 20:12:49 +0300

    Preact: Add polyfill DragDropTouch https://github.com/drag-drop-touch-js/dragdroptouch

--- play.pokemonshowdown.com/src-gens/client-endload.ts
+++ play.pokemonshowdown.com/src-gens/client-endload.ts
@@ -1,6 +1,13 @@
 import { PS } from "./client-main";
 
 PS.libsLoaded.loaded();
+
+if(window.DragDropTouch) {
+	window.DragDropTouch.enable(undefined, undefined, {
+		isPressHoldMode: true,
+	});
+}
+
 if(PS.prefs.showdex) {
 	const script = document.createElement('script');
 	script.src = 'showdex-gens/main.js';
