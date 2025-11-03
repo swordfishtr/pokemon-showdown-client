12ed727855f6c95d216b1c3ed06b50776a693d47
demir <demir.ab@protonmail.com>
2025-11-04 00:51:41 +0300

    fix sprite positioning

--- play.pokemonshowdown.com/src-gens/battle-dex.ts
+++ play.pokemonshowdown.com/src-gens/battle-dex.ts
@@ -859,16 +859,17 @@ export const Dex = new class implements ModdedDex {
 			const left = (num % 12) * 40;
 			const fainted = ((pokemon as Pokemon | ServerPokemon)?.fainted ?
 				`;opacity:.3;filter:grayscale(100%) brightness(.5)` : ``);
-			return `background:transparent url(${Dex.resourcePrefix}sprites/pokemonicons-sheet.png?v18) no-repeat scroll -${left}px -${top}px${fainted}`;
+			return `background:transparent url(${Dex.resourcePrefix}sprites/pokemonicons-sheet.png?v18) no-repeat scroll -${left}px -${top}px${fainted};`;
 		}
 		else {
 			// Generations custom species
 			const num = this.getGensPokemonIconNum(id, pokemon?.gender === 'F');
 			const top = Math.floor(num / 10) * 32;
-			const left = (num % 10) * 32;
+			let left = (num % 10) * 32;
+			if(!(top === 0 && left === 0)) left -= 4;
 			const fainted = ((pokemon as Pokemon | ServerPokemon)?.fainted ?
 				`;opacity:.3;filter:grayscale(100%) brightness(.5)` : ``);
-			return `background:transparent url(${Dex.resourcePrefix}sprites/gens-pokemonicons-sheet.png?v18) no-repeat scroll -${left}px -${top}px${fainted}`;
+			return `background:transparent url(${Dex.resourcePrefix}sprites/gens-pokemonicons-sheet.png?v18) no-repeat scroll -${left}px -${top}px${fainted}; width:32px;`;
 		}
 	}
 
