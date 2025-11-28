5794ca259d811dce0348752743942687da4615dd
demir <demir.ab@protonmail.com>
2025-11-29 01:03:00 +0300

    fix

--- play.pokemonshowdown.com/src-gens/panel-mainmenu.tsx
+++ play.pokemonshowdown.com/src-gens/panel-mainmenu.tsx
@@ -827,10 +827,11 @@ export class TeamForm extends preact.Component<{
 	submit = (ev: Event, validate?: 'validate') => {
 		ev.preventDefault();
 		const format = this.format;
+		const teambuilderFormat = PS.teams.teambuilderFormat(format);
 		const teamElement = this.base!.querySelector<HTMLButtonElement>('button[name=team]');
 		const teamKey = teamElement!.value;
 		const team = teamKey ? PS.teams.byKey[teamKey] : undefined;
-		if (!window.BattleFormats[toID(format)]?.team && !team) {
+		if (window.BattleFormats?.[teambuilderFormat] && !BattleFormats[teambuilderFormat].team && !team) {
 			PS.alert('You need to go into the Teambuilder and build a team for this format.', {
 				parentElem: teamElement!,
 			});
