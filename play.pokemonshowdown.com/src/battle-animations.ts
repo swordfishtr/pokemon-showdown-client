05465da29d81feb9144f415c1c330634498b9d2b
Marquis <160788133+mmtaccos@users.noreply.github.com>
2026-02-15 16:40:11 -0500

    Update battle-animations.ts

--- play.pokemonshowdown.com/src/battle-animations.ts
+++ play.pokemonshowdown.com/src/battle-animations.ts
@@ -273,6 +273,20 @@ export class BattleScene implements BattleSceneStub {
 		transition: string, after?: string, additionalCss?: JQuery.PlainObject
 	) {
 		if (typeof effect === 'string') effect = BattleEffects[effect];
+
+		let $effect = $(`<img src="${effect.url!}" style="display:block;position:absolute" />`);
+		this.$fx.append($effect);
+		if (additionalCss) $effect.css(additionalCss);
+		$effect = this.$fx.children().last();
+
+		return this.animateEffect($effect, effect, start, end, transition, after);
+	}
+	animateEffect(
+		$effect: JQuery, effect: string | SpriteData, start: ScenePos, end: ScenePos,
+		transition: string, after?: string, additionalCss?: JQuery.PlainObject
+	) {
+		if (typeof effect === 'string') effect = BattleEffects[effect];
+
 		if (!start.time) start.time = 0;
 		if (!end.time) end.time = start.time + 500;
 		start.time += this.timeOffset;
@@ -285,16 +299,13 @@ export class BattleScene implements BattleSceneStub {
 		let startpos = this.pos(start, effect);
 		let endpos = this.posT(end, effect, transition, start);
 
-		let $effect = $(`<img src="${effect.url!}" style="display:block;position:absolute" />`);
-		this.$fx.append($effect);
-		if (additionalCss) $effect.css(additionalCss);
-		$effect = this.$fx.children().last();
-
 		if (start.time) {
 			$effect.css({ ...startpos, opacity: 0 });
 			$effect.delay(start.time).animate({
 				opacity: startpos.opacity,
 			}, 1);
+		} else if ($effect.queue().length) {
+			$effect.animate(startpos, 0);
 		} else {
 			$effect.css(startpos);
 		}
@@ -313,6 +324,8 @@ export class BattleScene implements BattleSceneStub {
 			$effect.animate(endendpos, 200);
 		}
 		this.waitFor($effect);
+
+		return $effect;
 	}
 	backgroundEffect(bg: string, duration: number, opacity = 1, delay = 0) {
 		let $effect = $('<div class="background"></div>');
@@ -532,7 +545,7 @@ export class BattleScene implements BattleSceneStub {
 		let animEntry = BattleMoveAnims[moveid];
 		if (this.acceleration >= 3) {
 			const targetsSelf = !participants[1] || participants[0] === participants[1];
-			const isSpecial = !targetsSelf && this.battle.dex.moves.get(moveid).category === 'Special';
+			const isSpecial = !targetsSelf && this.battle.gtt.getFormatMove(moveid).category === 'Special';
 			animEntry = BattleOtherAnims[targetsSelf ? 'fastanimself' : isSpecial ? 'fastanimspecial' : 'fastanimattack'];
 		} else if (!animEntry) {
 			animEntry = BattleMoveAnims['tackle'];
@@ -563,9 +576,15 @@ export class BattleScene implements BattleSceneStub {
 	}
 
 	updateGen() {
-		let gen = this.battle.gen;
+		let gen = this.battle.gtt.dex.gen;
 		if (Dex.prefs('nopastgens')) gen = 6;
 		if (Dex.prefs('bwgfx') && gen > 5) gen = 5;
+		if([
+			'gen9regenerations10',
+		].includes(this.battle.gtt.formatid)) {
+			// Hardcoded to match custom sprites
+			gen = 5;
+		}
 		this.gen = gen;
 		this.activeCount = this.battle.nearSide?.active.length || 1;
 
@@ -586,10 +605,8 @@ export class BattleScene implements BattleSceneStub {
 			else if (gen <= 4) bg = 'fx/bg-gen4-spl.png';
 			else bg = 'fx/bg-spl.png';
 			this.setBgm(-101);
-		}
-		else if (typeof rated === 'string' && rated.startsWith("Prepare for Takeoff")) {
-			bg = 'fx/bg-space.jpg';
-		}
+		}	
+			
 		else if (typeof rated === 'string' && rated.startsWith('National Pokemon Association')) {
 			bg = 'fx/bg-npa.png';
 			this.setBgm(-101);
@@ -637,7 +654,7 @@ export class BattleScene implements BattleSceneStub {
 		return BattleLog.escapeHTML(name);
 	}
 	getSidebarHTML(side: Side, posStr: string): string {
-		let noShow = this.battle.hardcoreMode && this.battle.gen < 7;
+		let noShow = this.battle.hardcoreMode && this.battle.gtt.dex.gen < 7;
 
 		let speciesOverage = this.battle.speciesClause ? Infinity : Math.max(side.pokemon.length - side.totalPokemon, 0);
 		const sidebarIcons: (
@@ -932,7 +949,7 @@ export class BattleScene implements BattleSceneStub {
 			pWeather[1] = pWeather[2];
 			pWeather[2] = 0;
 		}
-		if (this.battle.gen < 7 && this.battle.hardcoreMode) return buf;
+		if (this.battle.gtt.dex.gen < 7 && this.battle.hardcoreMode) return buf;
 		if (pWeather[2]) {
 			return `${buf} <small>(${pWeather[1]} or ${pWeather[2]} turns)</small>`;
 		}
@@ -944,7 +961,7 @@ export class BattleScene implements BattleSceneStub {
 	sideConditionLeft(cond: Side['sideConditions'][string], isFoe: boolean, all?: boolean) {
 		if (!cond[2] && !cond[3] && !all) return '';
 		let buf = `<br />${isFoe && !all ? "Foe's " : ""}${Dex.moves.get(cond[0]).name}`;
-		if (this.battle.gen < 7 && this.battle.hardcoreMode) return buf;
+		if (this.battle.gtt.dex.gen < 7 && this.battle.hardcoreMode) return buf;
 
 		if (!cond[2] && !cond[3]) return buf;
 		if (!cond[2] && cond[3]) {
@@ -957,7 +974,7 @@ export class BattleScene implements BattleSceneStub {
 		return `${buf} <small>(${cond[2]} or ${cond[3]} turns)</small>`;
 	}
 	weatherLeft() {
-		if (this.battle.gen < 7 && this.battle.hardcoreMode) return '';
+		if (this.battle.gtt.dex.gen < 7 && this.battle.hardcoreMode) return '';
 
 		let weatherhtml = ``;
 
