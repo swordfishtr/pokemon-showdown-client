af3de77adc40c1a6ac1b5d7753d81b2a066502be
demir <demir.ab@protonmail.com>
2026-06-06 21:22:36 +0300

    Fix false positive 'custom' property

--- play.pokemonshowdown.com/src-gens/battle-dex-data.ts
+++ play.pokemonshowdown.com/src-gens/battle-dex-data.ts
@@ -1217,7 +1217,7 @@ export class Item implements Effect {
 		this.id = id;
 		this.gen = data.gen || 0;
 		this.exists = ('exists' in data ? !!data.exists : true);
-		if('custom' in data) this.custom = true;
+		if (data.custom) this.custom = true;
 
 		this.num = data.num || 0;
 		this.spritenum = data.spritenum || 0;
@@ -1357,7 +1357,7 @@ export class Move implements Effect {
 		this.id = id;
 		this.gen = data.gen || 0;
 		this.exists = ('exists' in data ? !!data.exists : true);
-		if('custom' in data) this.custom = true;
+		if (data.custom) this.custom = true;
 
 		this.basePower = data.basePower || 0;
 		this.accuracy = data.accuracy || 0;
@@ -1525,7 +1525,7 @@ export class Ability implements Effect {
 		this.id = id;
 		this.gen = data.gen || 0;
 		this.exists = ('exists' in data ? !!data.exists : true);
-		if('custom' in data) this.custom = true;
+		if (data.custom) this.custom = true;
 		this.num = data.num || 0;
 		this.shortDesc = data.shortDesc || data.desc || '';
 		this.desc = data.desc || data.shortDesc || '';
@@ -1618,7 +1618,7 @@ export class Species implements Effect {
 		this.id = id;
 		this.gen = data.gen || 0;
 		this.exists = ('exists' in data ? !!data.exists : true);
-		if ('custom' in data) this.custom = true;
+		if (data.custom) this.custom = true;
 		if (data.credits) this.credits = data.credits;
 		this.baseSpecies = data.baseSpecies || name;
 		this.forme = data.forme || '';
