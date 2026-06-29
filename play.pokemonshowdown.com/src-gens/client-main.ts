a124528105458948c46dca62ad1a45dd63bd0492
demir <demir.ab@protonmail.com>
2026-06-29 03:03:51 +0300

    Use system theme by default

--- play.pokemonshowdown.com/src-gens/client-main.ts
+++ play.pokemonshowdown.com/src-gens/client-main.ts
@@ -48,7 +48,7 @@ class PSPrefs extends PSStreamModel<string | null> {
 	/**
 	 * The theme to use. "system" matches the theme of the system accessing the client.
 	 */
-	theme: 'light' | 'dark' | 'system' = 'light';
+	theme: 'light' | 'dark' | 'system' = 'system';
 	/**
 	 * Disables animated GIFs, but keeps other animations enabled.
 	 * Workaround for a Chrome 64 bug with GIFs.
